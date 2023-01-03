30ae32b7d1d86307ec00d66bb6b16496c5e1c626
Tahsin <tahsin.anam@tietoevry.com>
2023-01-03 12:19:25 +0200

    initial commit

---
+++ src/main/java/producer/tahsin/kafka/simpleProducer.java
@@ -0,0 +1,15 @@
+package producer.tahsin.kafka;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class simpleProducer {
+
+    private static final Logger log = LoggerFactory.getLogger(simpleProducer.class);
+
+    public static void main(String[] args) {
+
+    }
+
+}
